f72386e112ed3cfcf39e6b41bfb4068ce4aa5346
Hugo Gonzalez Besada <hugogbmahu@gmail.com>
2025-02-17 14:53:00 +0100

    feat: is_dni_numero
    crer funcion para checkear la parte numerica

--- dni_java/src/main/java/com/codebind/demo/Dni.java
+++ dni_java/src/main/java/com/codebind/demo/Dni.java
@@ -1,6 +1,6 @@
 package com.codebind.demo;
 
-public class Dni  {
+public class Dni {
     private final String dni;
     private final int LONGITUD_DNI = 9;
     private Boolean numero = false;
@@ -12,28 +12,43 @@ public class Dni  {
         this.dni = dni;
     }
 
-    private String get_dni(){
+    private String get_dni() {
         return this.dni;
 
     }
-    private void String set_numero(Boolean value){
+
+    private void set_numero(Boolean value) {
         this.numero = value;
-
-
     }
-    private void String set_letra(Boolean value){
+
+    private void set_letra(Boolean value) {
         this.letra = value;
     }
 
-    private Boolean is_numero(){
+    private Boolean is_numero() {
         return this.numero;
     }
 
-
-    private Boolean is_letra(){
+    private Boolean is_letra() {
         return this.letra;
     }
 
+    private int get_longitud() {
+        return this.LONGITUD_DNI;
+    }
 
+    public String get_parte_numerica() {
+        return get_dni().substring(0, LONGITUD_DNI - 1);
+    }
+
+    public Boolean is_dni_numero(String dni) {
+        for (int i = 0; i < dni.length(); i++) {
+            if (Character.isDigit(dni.charAt(i))) {
+                return true;
+            } else
+                ;
+        }
+        return false;
+    }
 
 }
