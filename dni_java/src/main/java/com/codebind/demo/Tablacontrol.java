960b50ec8016705593ecd2a8d4174411a5e92348
Mahu2121 <hugogbmahu@gmail.com>
2025-02-18 00:02:30 +0100

    test:
    test tabla control calcular letra permitida

--- dni_java/src/main/java/com/codebind/demo/Tablacontrol.java
+++ dni_java/src/main/java/com/codebind/demo/Tablacontrol.java
@@ -7,34 +7,21 @@ public class Tablacontrol {
             'N', 'J', 'Z', 'S', 'Q', 'V',
             'H', 'L', 'C', 'K', 'E'};
 
-
-
-        char get_letra_tabla(int position) throws ArrayIndexOutOfBoundsException{
-            try{
-                return this.tablacontrol[position];
-            }
-
-            catch(ArrayIndexOutOfBoundsException exception){
-                throw new ArrayIndexOutOfBoundsException("The position is out of bounds");
-
-
-
-
-            }
+    char get_letra_tabla(int position) throws ArrayIndexOutOfBoundsException {
+        try {
+            return this.tablacontrol[position];
+        } catch (ArrayIndexOutOfBoundsException exception) {
+            throw new ArrayIndexOutOfBoundsException("The position is out of bounds");
+        }
     }
-        
 
+    private int get_modulo() {
+        return this.tablacontrol.length;
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
+    char calcular_letra(String DNI) {
+        int dni = Integer.parseInt(DNI);
+        int posicion = dni % get_modulo();
+        return get_letra_tabla(posicion);
+    }
 }
--- dni_java/src/test/java/com/codebind/demo/Tabla_Test.java
+++ dni_java/src/test/java/com/codebind/demo/Tabla_Test.java
@@ -1,8 +1,10 @@
 package com.codebind.demo;
 
+import java.util.List;
 import static org.junit.Assert.assertEquals;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.assertTrue;
 
 public class Tabla_Test {
 
@@ -12,11 +14,6 @@ public class Tabla_Test {
     public static void crearTabla() {
         tabla = new Tablacontrol();
     }
-
-
-
-
-
 
     @Test
     public void get_letra_tabla_Test() {
@@ -29,7 +26,31 @@ public class Tabla_Test {
         tabla.get_letra_tabla(100);
     }
 
+    private static boolean calcular_letra_tabla_Test(List<String> casos_test, Tablacontrol tabla){
 
+        for (String dni : casos_test) {
 
+            String parte_numerica_dni = dni.substring(0, dni.length() - 1);
+            char letra_dni = dni.charAt(dni.length() - 1);
+
+            if (tabla.calcular_letra( parte_numerica_dni ) == letra_dni) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void calcular_letra_permitida_Test() {
+
+        String[] casos_test = {
+                "78484464T","72376173A","01817200Q","95882054E","63587725Q",
+                "26861694V","21616083Q","26868974Y","40135330P","89044648X",
+                "80117501Z","34168723S","76857238R","66714505S","66499420A"};
+
+        assertTrue(calcular_letra_tabla_Test(List.of(casos_test), tabla));
+    }
 
 }
