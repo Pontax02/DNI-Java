34a8eb0293bd6cee20940e51a36876836d621766
Hugo Gonzalez Besada <hugogbmahu@gmail.com>
2025-02-17 14:53:44 +0100

    test:
    test parte numerica

--- dni_java/src/test/java/com/codebind/demo/Dni_Test.java
+++ dni_java/src/test/java/com/codebind/demo/Dni_Test.java
@@ -6,22 +6,21 @@ import static org.junit.Assert.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+
 public class Dni_Test {
-
-
-
 
    private static final String[] casos_test = {"78484464T","72376173A","01817200Q","95882054E","63587725Q",
            "26861694V","21616083Q","26868974Y","40135330P","89044648X",
            "80117501Z","34168723S","76857238R","66714505S","66499420A"};
 
+
     @Test
-    public void parte_numerica_test(){
+    public void parte_numerica_Test(){
         Dni objeto_dni = null;
         for(String dni : casos_test){
             objeto_dni = new Dni(dni);
             assertTrue(
-                    objeto_dni.check_parte_numerica(objeto_dni.get_parte_numerica())
+                    objeto_dni.is_dni_numero(objeto_dni.get_parte_numerica())
             );
 
 
